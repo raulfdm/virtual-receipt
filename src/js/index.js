ced2b4af1c5f6ec3190782e809f43edf5dd62352
Raul F. de Melo <melo.raulf@gmail.com>
2017-05-05 10:41:00 +0100

    Changed for chain function (old class mode)

--- src/js/index.js
+++ src/js/index.js
@@ -1,150 +1,112 @@
-/*
-1. Get the values from the inputs
-2. Get others elements from page
-  2.1 Insert Button
-  2.2 List
-3. Add event to elements that need it
-4. Create a function to mount an object with this values (CHAIN FUNCTION)
-  4.1 Object containing the following properties:
-    4.1.1 name
-    4.1.2 quantity
-    4.1.3 Price
-    4.1.4 Total
-    4.1.5 htmlELEMENT
-5. Save an Global array to store the products
-6. Create a function to save into Array and into a LocalStore
-7. Create a function to clean the array and LocalStorage
-8. Create a function to fill the List container with all elements from Array of Products
-  8.1 Call this function with the first load
-9. Create a function to fill the TOTAL
-10. Create function to clean inputs (focus on first elem)
-*/
-'use-strict'
-//debugger;
-// 1. Get the values from the inputs
-const $productName = document.querySelector('#product-name')
-const $productQtde = document.querySelector('#product-qtde')
-const $productPrice = document.querySelector('#product-price')
-const $productList = document.querySelector('.product-list')
-
-// 2. Get others elements from page
-const $formProduct = document.querySelector('#form-product')
-const $totalProducts = document.querySelector('#container-list__display-total__value')
-const $btnInsertProduct = document.querySelector('#btn-insert-product')
-const $btnCleanList = document.querySelector('.container-list__btn-clean')
-
-// 5. Save an Global array to store the products
-let listOfProducts
-
-// 3. Add event to elements that will need it
-$formProduct.addEventListener('submit', function (e) {
-  e.preventDefault()
-  const product = createProduct({name: $productName.value, price: $productPrice.value, quantity: $productQtde.value})
-  saveProduct(product)
-  loadGrid()
-  cleanInputs()
-})
-
-$btnCleanList.addEventListener('click', function (e) {
-  const result = confirm('Do you really want to clean the list?')
-  if (result) {
-    cleanProductList()
-    updateTotal()
-  }
-})
-
-// 4. Create a function to mount a Product with this values (CHAIN FUNCTION)
-const createeProduct = function (product) {
-  return new Product()
-    .setName(product.name)
-    .setPrice(product.price)
-    .setQuantity(product.quantity)
-    .generateId()
-    .calculateTotal()
+const application = function () {
+  this._localStorage = 'list_of_products'
+  this._productList = []
+  this.product = {}
+  this.total = 0.0
+  this._veryLocalStorage()
 }
-
-// 6. Create a function to save into Array and into a LocalStore
-const saveProduct = function (product) {
-  listOfProducts.push(product)
-  localStorage.setItem('listOfProducts', JSON.stringify(listOfProducts))
-}
-// 7. Create a function to clean the array and LocalStora
-const cleanProductList = function () {
-  let arrayOfProductNodes = [...document.querySelectorAll('.product-list__item')]
-
-  arrayOfProductNodes.map(node => node.remove())
-  listOfProducts.length = 0
-  localStorage.removeItem('listOfProducts')
-}
-
-// 8. Create a function to fill the List container with all
-const fillListProductContainer = function (nodeParent) {
-  if (listOfProducts) {
-    const html = listOfProducts.reduce((result, product) => {
-      return result + product.getInnerHTML()
-    }, '')
-    nodeParent.innerHTML = html
-  }
-}
-
-const loadLocalList = function () {
-  let list = JSON.parse(localStorage.getItem('listOfProducts'))
-  if (!list) {
-    return []
-  } else {
-    return list.map(product => createProduct(product))
-  }
-}
-const cleanInputs = function () {
-  $productName.value = ''
-  $productName.focus()
-  $productPrice.value = ''
-  $productQtde.value = '1'
-}
-
-const loadGrid = function () {
-  fillListProductContainer($productList)
-  addEventToBtnRemove()
-  updateTotal()
-}
-
-const updateTotal = function () {
-  const newTotal = listOfProducts.reduce((amount, product) => amount + getDecimalFix(product.total), 0.0)
-  $totalProducts.textContent = getDecimalFix(newTotal);
-}
-
-const addEventToBtnRemove = function () {
-  const btnRemoveElements = [...document.querySelectorAll('.btn-remove-product')]
-  btnRemoveElements.map(btn => {
-    btn.addEventListener('click', removeSpecificElement)
-  })
-}
-
-const removeSpecificElement = function (event) {
-  const answer = confirm('Do you really want to remove this item?');
-  if (answer && event.currentTarget.nodeName==="BUTTON") {
-    const parent = event.currentTarget.parentNode
-  
-    const id = parent.dataset.id
-    const newProductList = listOfProducts.filter(product => product.id !== id)
-    parent.remove()
-    updateList(newProductList)
-    updateTotal()
-  }
-}
-
-const updateList = function (newProductList) {
-  listOfProducts = newProductList
-  localStorage.setItem('listOfProducts', JSON.stringify(listOfProducts))
-}
-
-const boostrap = function () {
-  listOfProducts = loadLocalList()
-  loadGrid()
-}
-
-const getDecimalFix = function(value){
+application.prototype.getDecimal = function (value) {
   return parseFloat(value.toFixed(2))
 }
+application.prototype._veryLocalStorage = function () {
+  const list = JSON.parse(localStorage.getItem(this._localStorage))
+  if (list) {
+    list.map(product => this.createProduct(product)._saveProduct())
+    this.loadGrid()
+  }
+}
+application.prototype.createProduct = function (product) {
+  debugger
+  this.product = new Product()
+    .setName(product.name)
+    .setQuantity(product.quantity)
+    .setPrice(product.price)
+    .calculateMethod(product.isPerItem ? true : false)
+    .generateId()
+    .calculateTotal()
 
-boostrap()
+  return this
+}
+application.prototype._saveProduct = function () {
+  this
+    ._productList
+    .push(this.product)
+  localStorage.setItem(this._localStorage, JSON.stringify(this._productList))
+  return this
+}
+application.prototype._resetProduct = function () {
+  this.product = new Product()
+  return this
+}
+application.prototype.cleanProductList = function () {
+  let arrayOfProductNodes = [...document.querySelectorAll('.product-list__item')]
+  arrayOfProductNodes.map(node => node.remove())
+  this._productList.length = 0
+  localStorage.removeItem(this._localStorage)
+  return this
+}
+application.prototype.loadGrid = function () {
+  const $listContent = document.querySelector('.list-content')
+  const result = this._productList.reduce((prev, product) => prev += product.getInnerHTML(), '')
+  $listContent.innerHTML = result
+  this._updateTotal()
+  return this
+}
+application.prototype._updateTotal = function () {
+  const $totalBill = document.querySelector('.list-footer__total-value')
+  let newTotal = 0.0
+  if (this._productList.length > 0) {
+    newTotal = this
+      ._productList
+      .reduce((amount, product) => this.getDecimal(amount + product.total), 0.0)
+  }else {
+    newTotal = '0.00'
+  }
+  $totalBill.textContent = newTotal
+
+  return this
+}
+application.prototype.cleanInputs = function () {
+  document
+    .querySelector('#product-name')
+    .value = ''
+  document
+    .querySelector('#product-qtde')
+    .value = 1
+  document
+    .querySelector('#product-price')
+    .value = ''
+}
+
+/*PENDING*/
+application.prototype.removeSpecificElement = function () {}
+
+const $buttonInsertItem = document.querySelector('#btn-insert')
+const $buttonCleanList = document.querySelector('#btn-clean')
+
+const app = new application()
+
+$buttonInsertItem.addEventListener('click', function (event) {
+  const $inputs = {
+    name: document.querySelector('#product-name'),
+    qtde: document.querySelector('#product-qtde'),
+    price: document.querySelector('#product-price'),
+    calcMethod: document.querySelector('#product-method__item')
+  }
+
+  app
+    .createProduct({
+      name: $inputs.name.value,
+      quantity: $inputs.qtde.value,
+      price: $inputs.price.value,
+      isPerItem: $inputs.calcMethod.checked
+    })
+    ._saveProduct()
+    ._resetProduct()
+    .loadGrid()
+    .cleanInputs()
+})
+
+$buttonCleanList.addEventListener('click', function (event) {
+  app.cleanProductList().loadGrid()
+})
